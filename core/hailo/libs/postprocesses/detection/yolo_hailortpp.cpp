7873ccab573cff2cd025ee7138bb8b6680e225f0
maengjemo <maengjemo@gm.gist.ac.kr>
2025-08-12 19:05:56 +0900

    Fix: yolo_hailortpp.cpp work well

--- core/hailo/libs/postprocesses/detection/yolo_hailortpp.cpp
+++ core/hailo/libs/postprocesses/detection/yolo_hailortpp.cpp
@@ -17,8 +17,8 @@ static std::map<uint8_t, std::string> yolo_vehicles_labels = {
 static std::map<uint8_t, std::string> yolo_3class_labels = {
     {0, "unlabeled"},
     {1, "Person"},
-    {2, "Door"}},
-    {3, "Window"};
+    {2, "Door"},
+    {3, "Window"});
 
 static std::map<uint8_t, std::string> yolo_10class_labels = {
     {0, "unlabeled"},
